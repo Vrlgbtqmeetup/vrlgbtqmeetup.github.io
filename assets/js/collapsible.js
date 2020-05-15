f1b4da01ff5392359c084f967e63138d1ffa34df
LunarTiger <lunartiger@live.com>
2020-05-14 20:43:40 -0400

    :tada:

--- assets/js/collapsible.js
+++ assets/js/collapsible.js
@@ -1,7 +1,8 @@
 var coll = document.getElementsByClassName("collapsible");
 var default_open = document.getElementById('openme');
-var open_this = false;
 var i;
+var open_this = null;
+if(window.location.hash) {open_this = document.getElementById(document.getElementById(window.location.hash.substr(1)).dataset.parent);}
 if (coll) {
 	for (i = 0; i < coll.length; i++) {
 		coll[i].addEventListener("click", function() {
@@ -12,6 +13,5 @@ if (coll) {
 		});
 	}
 }
-if(window.location.hash) {open_this = document.getElementById(document.getElementById(window.location.hash.substr(1)).dataset.parent);}
-if (default_open) {default_open.click();}
-else if (open_this) {open_this.click();}
+if (open_this) {open_this.click();}
+else if (default_open) {default_open.click();}
